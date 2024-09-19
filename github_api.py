6c2b87833cff5ac6372ca485938f76470030e13f
alessiogiovagnini <alessio.giovagnini@usi.ch>
2024-09-19 15:35:25 +0200

    added small example

--- github_api.py
+++ github_api.py
@@ -1,4 +1,5 @@
 from github import Github
+from github import Auth
 from dotenv import load_dotenv
 import os
 
@@ -6,5 +7,10 @@ load_dotenv()
 API_KEY: str = os.getenv("API_KEY")
 
 if __name__ == '__main__':
-    print(API_KEY)
+    auth = Auth.Token(API_KEY)
+
+    github_instance: Github = Github(auth=auth)
+
+    for repo in github_instance.get_user().get_repos():
+        print(repo.name)
 
